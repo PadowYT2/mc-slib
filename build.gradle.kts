7c67285006e4af1b74c729386dc6d18e611aba2e
PadowYT2 <me@padow.ru>
2025-03-11 22:12:00 +0300

    lets try this

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,7 @@ subprojects {
         java {
             withSourcesJar()
 
-            toolchain.languageVersion.set(JavaLanguageVersion.of(8))
+            toolchain.languageVersion.set(JavaLanguageVersion.of(21))
         }
 
         compileJava {
